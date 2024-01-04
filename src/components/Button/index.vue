1e3e78a795305b08bbc0f39e4aa71148ae0ec79c
Илья <pichugin@team.trueconf.com>
2024-01-04 19:19:37 +0300

    Добавлен эффект клика на кнопки

--- src/components/Button/index.vue
+++ src/components/Button/index.vue
@@ -11,26 +11,11 @@
         {{ icon }}
       </span>
     </slot>
-
-    <span class="mdc-button__ripple"/>
-    <!-- <div
-    class="button-component__ripple-container">
-      <div
-      class="button-component__ripple"
-      ref="item"/>
-    </div> -->
-
-    <!-- <div class="mdc-ripple-surface button-component__ripple-container">
-      <div
-      class="button-component__ripple"
-      ref="item"/>
-    </div> -->
   </button>
 </template>
 
 <script lang="ts">
-  import { defineComponent, onMounted } from 'vue';
-  // import { MDCRipple } from '@material/ripple';
+  import { defineComponent } from 'vue';
 
   export default defineComponent({
     name: 'ButtonComponent',
@@ -44,34 +29,11 @@
         default: null,
       },
     },
-    setup() {
-      // const rippleEffect = ref<MDCRipple>();
-      // const item = ref<Element>();
-
-      // function updateRipple() {
-      //   if (item.value) {
-      //     rippleEffect.value?.destroy();
-      //     console.log(item.value);
-
-      //     rippleEffect.value = new MDCRipple(item.value);
-      //   }
-      // }
-
-      onMounted(() => {
-        // updateRipple();
-      });
-
-      return {
-      };
-    },
   });
 </script>
 
 <style lang="scss">
   @use '@/styles/variables' as *;
-  @use 'sass:color';
-  @use '@material/ripple';
-  @use '@material/theme/index.scss' as mdc-theme;
 
   .button-component {
     position: relative;
@@ -89,82 +51,32 @@
 
     cursor: pointer;
 
-    // &::after {
-    //   position: absolute;
-    //   top: 0;
-    //   left: 0;
-    //   z-index: 10;
+    &::after {
+      position: absolute;
+      top: 0;
+      left: 0;
 
-    //   display: block;
+      width: 100%;
+      height: 100%;
 
-    //   width: 100%;
-    //   height: 100%;
+      background-color: rgb(0 0 0 / 0.05);
+      opacity: 0;
 
-    //   // background-color: rgba(#000, 0.05);
-    //   opacity: 0;
+      content: '';
 
-    //   content: '';
+      transition:
+        opacity 0.1s linear,
+        background-color 0.05s linear;
+    }
 
-    //   transition: all 0.1s linear;
-    // }
+    &:hover::after {
+      opacity: 1;
+    }
 
-    // &:hover::after {
-    //   opacity: 1;
-    // }
+    &:active::after {
+      background-color: rgb(0 0 0 / 0.1);
+    }
   }
-
-  .mdc-button__ripple {
-    display: block;
-  }
-
-  .mdc-button__ripple::before,
-  .mdc-button__ripple::after {
-    background-color: red;
-  }
-
-  // .button-component__ripple-container {
-  //   position: absolute;
-  //   top: 0;
-  //   left: 0;
-
-  //   width: 100%;
-  //   height: 100%;
-  //   overflow: hidden;
-
-  //   // border-radius: 14px;
-  // }
-
-  // .button-component__ripple {
-  //   @include ripple.surface;
-  //   @include ripple.radius-bounded;
-  //   @include ripple.states;
-
-  //   // @include ripple.states-opacities(
-  //   //   (
-  //   //     hover: 0,
-  //   //     focus: 0.1,
-  //   //     press: 0.1,
-  //   //   )
-  //   // );
-
-  //   &:hover {
-  //     background-color: rgba(#000, 0.03);
-  //   }
-
-  //   width: 100%;
-  //   min-height: 100%;
-  // }
-
-  // .button-component__ripple::before,
-  // .button-component__ripple::after {
-  //   @if mdc-theme.tone($color-app-primary) == 'dark' {
-  //     background-color: white !important;
-  //   } @else {
-  //     background-color: black !important;
-  //   }
-
-  //   // background-color: #fff;
-  // }
 
   .button-component__icon {
     font-size: 48px !important;
